6b9db6d9e743ca989af48069ef42f1edcdb6b34e
rschnurrpusch <31864213+rschnurrpusch@users.noreply.github.com>
2025-02-14 21:08:36 -0800

    250214-19.17 Foundation of a custom hero component (#1)
    * 250214-19.17 Foundation of a custom hero component
    
    * 250214-19:28 Reverting changes to scripts.js
    
    * 250214-19:52 Removing default hero again
    
    * 250217-20:29 Added in conditional check for custom hero before rendering hero default
    
    * 250214-20:37 Eliminating build complaints
    
    * 250214-20:46 Solving very minor QA issues
    
    * 250214-20:48 Reverting change to header.js from nav effort
    
    ---------
    
    Co-authored-by: Ryan Schnurrpusch <ryan.schnurrpusch@Ryans-MacBook-Pro.local>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -62,13 +62,16 @@ const pluginContext = {
  * @param {Element} main The container element
  */
 function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
+  // if no custom hero, render default hero
+  if (!document.querySelector('.hero-custom')) {
+    const h1 = main.querySelector('h1');
+    const picture = main.querySelector('picture');
+    // eslint-disable-next-line no-bitwise
+    if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+      const section = document.createElement('div');
+      section.append(buildBlock('hero', { elems: [picture, h1] }));
+      main.prepend(section);
+    }
   }
 }
 
