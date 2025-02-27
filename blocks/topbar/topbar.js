10368a4f3cee5a386ba39ee96cf115f56a3d59d2
Ryan Schnurrpusch <ryan.schnurrpusch@concentrix.com>
2025-02-27 08:59:51 -0800

    250227-09:00 Updating topbar logic

--- blocks/topbar/topbar.js
+++ blocks/topbar/topbar.js
@@ -1,10 +1,11 @@
 export default async function decorate(block) {
   [...block.children].forEach((row) => {
-    // console.log(row);
+    console.log(row);
   });
 
   const topBarImage = block.querySelector('img');
-
+  const links = block.querySelectorAll('a');
+  const paragraphs = block.querySelectorAll('p');
   const topBarMarkup = `
     <div class="topbar">
         <div class="top-main">
@@ -12,11 +13,11 @@ export default async function decorate(block) {
                 <div class="flag">
                     <a class="dark-header-link"><img alt="country-Logo-flag" class="country-flag" src="${topBarImage.src}"></a>
                 </div>
-                <div class="dark-header-link"><a href="https://www.concentrix.com/adobe-summit-2025/">Contact</a></div>
-                <div class="dark-header-link"><a>Support</a></div>
+                <div class="dark-header-link"><a href="${links[0].href}">${links[0].textContent}</a></div>
+                <div class="dark-header-link"><a href="${links[1].href}">${links[1].textContent}</a></div>
             </div>
             <div class="shop-msg dark-header-link">
-                <a>Free shipping on orders over $50</a>
+                ${paragraphs[paragraphs.length - 1].textContent}
             </div>
             <div class="shop-links">
                 <div class="dark-header-link"><a></a></div>
